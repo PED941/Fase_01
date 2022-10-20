ecc803cfba4cba7becf0d22cfdb5588117c106bb
Marilyn Sorto <marelynivetteguzman@gmail.com>
2022-10-19 19:25:19 -0600

    avanze1
    diseño, parte del objeto usuario

---
+++ Gimnasio/Gimnasio/Usuarios.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Gimnasio
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Usuarios));
+            this.textBoxBusqueda = new System.Windows.Forms.TextBox();
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.buttonModificar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.buttonCuenta = new System.Windows.Forms.Button();
+            this.buttonIngresos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxBusqueda
+            // 
+            this.textBoxBusqueda.BackColor = System.Drawing.Color.FloralWhite;
+            this.textBoxBusqueda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBusqueda.ForeColor = System.Drawing.Color.FloralWhite;
+            this.textBoxBusqueda.Location = new System.Drawing.Point(117, 15);
+            this.textBoxBusqueda.Name = "textBoxBusqueda";
+            this.textBoxBusqueda.Size = new System.Drawing.Size(216, 20);
+            this.textBoxBusqueda.TabIndex = 1;
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAgregar.AutoSize = true;
+            this.buttonAgregar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonAgregar.BackColor = System.Drawing.SystemColors.ControlText;
+            this.buttonAgregar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonAgregar.BackgroundImage")));
+            this.buttonAgregar.FlatAppearance.BorderSize = 0;
+            this.buttonAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAgregar.Font = new System.Drawing.Font("Segoe Print", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAgregar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonAgregar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonAgregar.Location = new System.Drawing.Point(335, 303);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(63, 34);
+            this.buttonAgregar.TabIndex = 2;
+            this.buttonAgregar.Text = "Nuevo";
+            this.buttonAgregar.UseVisualStyleBackColor = false;
+            // 
+            // buttonModificar
+            // 
+            this.buttonModificar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonModificar.AutoSize = true;
+            this.buttonModificar.BackColor = System.Drawing.SystemColors.ControlText;
+            this.buttonModificar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonModificar.BackgroundImage")));
+            this.buttonModificar.FlatAppearance.BorderSize = 0;
+            this.buttonModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonModificar.Font = new System.Drawing.Font("Segoe Print", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonModificar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonModificar.Location = new System.Drawing.Point(425, 304);
+            this.buttonModificar.Name = "buttonModificar";
+            this.buttonModificar.Size = new System.Drawing.Size(84, 33);
+            this.buttonModificar.TabIndex = 3;
+            this.buttonModificar.Text = "Modificar";
+            this.buttonModificar.UseVisualStyleBackColor = false;
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.AutoSize = true;
+            this.buttonEliminar.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.buttonEliminar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonEliminar.BackgroundImage")));
+            this.buttonEliminar.FlatAppearance.BorderSize = 0;
+            this.buttonEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEliminar.Font = new System.Drawing.Font("Segoe Print", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEliminar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonEliminar.Location = new System.Drawing.Point(532, 305);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(93, 33);
+            this.buttonEliminar.TabIndex = 4;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = false;
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.AutoSize = true;
+            this.buttonSalir.BackColor = System.Drawing.SystemColors.ControlText;
+            this.buttonSalir.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonSalir.BackgroundImage")));
+            this.buttonSalir.FlatAppearance.BorderSize = 0;
+            this.buttonSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSalir.Font = new System.Drawing.Font("Segoe Print", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSalir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSalir.Location = new System.Drawing.Point(199, 304);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(97, 33);
+            this.buttonSalir.TabIndex = 5;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = false;
+            // 
+            // buttonCuenta
+            // 
+            this.buttonCuenta.AutoSize = true;
+            this.buttonCuenta.BackColor = System.Drawing.SystemColors.ControlText;
+            this.buttonCuenta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonCuenta.BackgroundImage")));
+            this.buttonCuenta.FlatAppearance.BorderSize = 0;
+            this.buttonCuenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCuenta.Font = new System.Drawing.Font("Segoe Print", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCuenta.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonCuenta.Location = new System.Drawing.Point(651, 305);
+            this.buttonCuenta.Name = "buttonCuenta";
+            this.buttonCuenta.Size = new System.Drawing.Size(137, 33);
+            this.buttonCuenta.TabIndex = 6;
+            this.buttonCuenta.Text = "Estado de Cuenta";
+            this.buttonCuenta.UseVisualStyleBackColor = false;
+            // 
+            // buttonIngresos
+            // 
+            this.buttonIngresos.AutoSize = true;
+            this.buttonIngresos.BackColor = System.Drawing.SystemColors.ControlText;
+            this.buttonIngresos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonIngresos.BackgroundImage")));
+            this.buttonIngresos.FlatAppearance.BorderSize = 0;
+            this.buttonIngresos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonIngresos.Font = new System.Drawing.Font("Segoe Print", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIngresos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonIngresos.Location = new System.Drawing.Point(12, 304);
+            this.buttonIngresos.Name = "buttonIngresos";
+            this.buttonIngresos.Size = new System.Drawing.Size(144, 33);
+            this.buttonIngresos.TabIndex = 7;
+            this.buttonIngresos.Text = "Listado de Ingresos";
+            this.buttonIngresos.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Buscar";
+            // 
+            // Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 345);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonIngresos);
+            this.Controls.Add(this.buttonCuenta);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonModificar);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.textBoxBusqueda);
+            this.Name = "Usuarios";
+            this.Text = "Usuarios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxBusqueda;
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.Button buttonModificar;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.Button buttonCuenta;
+        private System.Windows.Forms.Button buttonIngresos;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
